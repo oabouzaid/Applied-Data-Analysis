94ee7a892e9103d7163154dc2056b5c7758c27e8
Oussama Abouzaid <oussama6703@gmail.com>
2017-12-19 10:57:29 +0100

    OA_NS merged code

--- project/implementations.py
+++ project/implementations.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 import folium
 import json
 import branca.colormap as cm
+from ipywidgets import interact, interactive, fixed, interact_manual
 
 ''' N.B: PatentsView.org API does not allow us to get more than 100'000 per request.
     In case there is more than 100'000 patents, we will divide our request into 2 chunks, namely 2 times 6 months,
@@ -205,36 +206,117 @@ def spider_chart(df, index, title=''):
 
 #A typical IPC symbol is H01L 31, H=section 01=classification 31=group
 #Look at http://www.wipo.int/classifications/ipc/en/ to find the IPC symbols of a category
-def get_patents_keywords(key_words,year, section, classification, group):
+
+def query_ipc_by(field, flag):
+    query = '{"_or":['
+    for i,val in enumerate(field):
+        query += '{"_eq":{' + flag + ':"'+ val +'"}}'
+        if i != len(field) - 1:
+            query += ','
+    query += ']}'
+    return query
+
+def get_patents_keywords(key_words,year):
+    ''' Returns all the patents containing the given keywords, for a given year''' 
+    
+    #Year query parameters
+    query_year = '{"_gte":{"patent_date":"'+year+'-01-01"}},{"_lt":{"patent_date":"'+str(int(year)+1)+'-01-01"}}'
+  
+    nb_patents=0
+    dfPatents=pd.DataFrame()
+    #Send a query for every keyword
+    for i in key_words:         
+        query_key_words='{"_text_phrase":{"patent_title":"'+i+'"}}'
+        query='q={"_and":['+query_year+','+query_key_words+']}' 
+        output='&f=["patent_title","patent_number"]' 
+        
+        #Exception handler in case no patent is found for a given keyword
+        try:
+            r = requests.get(BASE_URL()+query+output).json()
+            nb_patents+=pd.DataFrame(r).total_patent_count[0] 
+            dfPatents=pd.concat([dfPatents,pd.DataFrame(r)])
+        except ValueError:
+            pass   
+    return dfPatents, nb_patents
+
+#Get patents by IPC, and then filter them by keywords                                                                       (used for FinTech)
+def get_patents_keywords_ipc(keywords, year, list_ipc):
     ''' Returns all the patents containing the given keywords, for a given year'''
 
     #Year query parameters
     query_year = '{"_gte":{"patent_date":"'+year+'-01-01"}},{"_lt":{"patent_date":"'+str(int(year)+1)+'-01-01"}}'
 
-    query_section = '{"_or":['
-    for i,val in enumerate(section):
-        query_section += '{"_eq":{"ipc_section":"'+val+'"}}'
-        if i != len(section)-1:
-            query_section += ','
-    query_section += ']}'
-
-    query_classification = '{"_or":['
-    for i,val in enumerate(classification):
-        query_classification += '{"_eq":{"ipc_class":"'+val+'"}}'
-        if i != len(classification)-1:
-            query_classification += ','
-    query_classification += ']}'
-
-    query_group = '{"_or":['
-    for i,val in enumerate(group):
-        query_group += '{"_eq":{"ipc_main_group":"'+val+'"}}'
-        if i != len(group)-1:
-            query_group += ','
-    query_group += ']}'
-
     nb_patents=0
     dfPatents=pd.DataFrame()
     #Send a query for every keyword
+    for (section, classification, group) in list_ipc:
+        query_group = '{"_eq":{"ipc_main_group":"'+group+'"}}'
+        query_section = '{"_eq":{"ipc_section":"'+section+'"}}'
+        query_classification = '{"_eq":{"ipc_class":"'+classification+'"}}'
+        
+        query='q={"_and":['+query_year+','+query_section+','+query_classification+','+query_group+']}'
+        output='&f=["patent_title","patent_number","ipc_section","ipc_main_group","ipc_class","patent_num_cited_by_us_patents",\
+                      "assignee_country", "assignee_organization", "patent_date",\
+                      "inventor_first_name", "inventor_last_name", "inventor_id"]'
+
+
+        option='&o={"per_page":10000}'
+        #Exception handler in case no patent is found for a given keyword
+        try:
+            r = requests.get(BASE_URL()+query+output+option).json()
+            nb_patents+=pd.DataFrame(r).total_patent_count[0]
+            dfPatents=pd.concat([dfPatents,pd.DataFrame(r)],ignore_index=True)
+        except ValueError:
+            pass
+
+    #Clean the dataframe
+    dfPatents.reindex(list(range(len(dfPatents))))
+    
+    columns = ["patent_title","patent_number", "IPCs", "patent_num_cited_by_us_patents","assignees",\
+               "patent_date", "inventors"]
+
+    dfPatents_cleaned=pd.DataFrame(columns=columns)
+    for col in columns:
+        dfPatents_cleaned[col]=list(map(lambda x: x[col], dfPatents.patents))
+
+    
+    filter_list=[]
+    keyWordFound=False
+    for title in dfPatents_cleaned.patent_title:
+        for tuple_keyword in keywords:
+            for i in range(len(tuple_keyword)):
+                if tuple_keyword[i] not in title:
+                    keyWordFound=False
+                    break                  
+                if i == len(tuple_keyword)-1:
+                    keyWordFound=True                    
+            if keyWordFound==True:
+                break
+        filter_list.append(keyWordFound)
+     
+    dfPatents_cleaned = dfPatents_cleaned.loc[filter_list]
+    return dfPatents_cleaned, len(dfPatents_cleaned)
+
+
+##########################################################################################
+#                    THIS FUNCTION IS USELESS
+##########################################################################################
+
+def get_patents_keywords_fil(key_words, year, section, classification, group):
+    ''' Returns all the patents containing the given keywords, for a given year'''
+
+    #Year query parameters
+    query_year = '{"_gte":{"patent_date":"'+year+'-01-01"}},{"_lt":{"patent_date":"'+str(int(year)+1)+'-01-01"}}'
+
+    #Query all the three IPC parts
+    query_section = query_by(section, "ipc_section")
+    query_classification = query_by(classification, "ipc_class")
+    query_group = query_by(group, "ipc_main_group")
+
+    nb_patents=0
+    dfPatents=pd.DataFrame()
+    
+    #Query each keyword
     for i in key_words:
         query_key_words='{"_text_phrase":{"patent_title":"'+i+'"}}'
         query='q={"_and":['+query_year+','+query_key_words+','+query_section+','+query_classification+','+query_group+']}'
@@ -259,34 +341,22 @@ def get_patents_keywords(key_words,year, section, classification, group):
 
     return [dfPatents_cleaned, nb_patents]
 
+##########################################################################################
+
+
 #A typical IPC symbol is H01L 31, H=section 01=classification 31=group
 #Look at http://www.wipo.int/classifications/ipc/en/ to find the IPC symbols of a category
-def get_patents_2keywords(key_words1,key_words2,year, section, classification, group):
+
+#Used for solar energy part (by IPC)
+def get_patents_keywords_ipc_sk(key_words1, key_words2, year, section, classification, group):
     ''' Returns all the patents containing the given keywords, for a given year'''
 
     #Year query parameters
     query_year = '{"_gte":{"patent_date":"'+year+'-01-01"}},{"_lt":{"patent_date":"'+str(int(year)+1)+'-01-01"}}'
 
-    query_section = '{"_or":['
-    for i,val in enumerate(section):
-        query_section += '{"_eq":{"ipc_section":"'+val+'"}}'
-        if i != len(section)-1:
-            query_section += ','
-    query_section += ']}'
-
-    query_classification = '{"_or":['
-    for i,val in enumerate(classification):
-        query_classification += '{"_eq":{"ipc_class":"'+val+'"}}'
-        if i != len(classification)-1:
-            query_classification += ','
-    query_classification += ']}'
-
-    query_group = '{"_or":['
-    for i,val in enumerate(group):
-        query_group += '{"_eq":{"ipc_main_group":"'+val+'"}}'
-        if i != len(group)-1:
-            query_group += ','
-    query_group += ']}'
+    query_section = query_by(section, "ipc_section")
+    query_classification = query_by(classification, "ipc_class")
+    query_group = query_by(group, "ipc_main_group")
 
     nb_patents=0
     dfPatents=pd.DataFrame()
@@ -301,8 +371,6 @@ def get_patents_2keywords(key_words1,key_words2,year, section, classification, g
             try:
                 r = requests.get(BASE_URL()+query+output+option).json()
                 nb_patents+=pd.DataFrame(r).total_patent_count[0]
-                if nb_patents > 10000:
-                    print('nb of patents too big (>10000)')
                 dfPatents=pd.concat([dfPatents,pd.DataFrame(r)],ignore_index=True)
             except ValueError:
                 pass
@@ -315,3 +383,181 @@ def get_patents_2keywords(key_words1,key_words2,year, section, classification, g
         dfPatents_cleaned[col]=list(map(lambda x: x[col], dfPatents.patents))
 
     return [dfPatents_cleaned, nb_patents]
+
+
+def get_nb_patent_years_keyword(years,keywords,list_ipc):
+    list_patent_nb = []
+    df = pd.DataFrame()
+    for i in years:
+        if (i != 2005): 
+            dfPatent, nb_patent = get_patents_keywords_ipc(keywords,str(i),list_ipc)
+            list_patent_nb.append(nb_patent)
+            df= pd.concat([df,dfPatent],ignore_index=True)
+    return list_patent_nb, df
+
+
+def plt_nb_patent(x,y,x_axis,y_axis,title):
+    plt.plot(x,y)
+    plt.xlabel(x_axis)
+    plt.ylabel(y_axis)
+    plt.title(title)
+    plt.grid(True, 'major', 'y', ls='--', lw=.5, c='k', alpha=.3)
+
+
+def savedfexcel(df, excel_title):
+    writer = pd.ExcelWriter('{0}.xlsx'.format(excel_title))
+    df.to_excel(writer,'Sheet1')
+    writer.save()
+
+
+def get_growth(years, list_nb_patents):
+    growth=[]
+    for i in range(len(years)-1):
+        growth+=[(list_nb_patents[i+1]-list_nb_patents[i])/list_nb_patents[i]*100]
+    return growth
+
+
+def build_dataframe(years,keywords,list_ipc):
+    list_patent_nb=[]
+    dfPatents=pd.DataFrame()
+    for i in years:
+        [dfPatentsYear, nb_patent]=get_patents_keywords_energy(keywords,str(i),list_ipc)
+        dfPatentsYear['year']=i
+        dfPatents=pd.concat([dfPatentsYear,dfPatents],ignore_index=True)
+        dfPatents.set_index([list(range(len(dfPatents)))],inplace=True)
+    return dfPatents
+
+
+def df_get_nb_by_group_and_clean(df, group, years, showtotal=False):
+    dfcopy=df.copy()
+    dfcopy['nb patent']=1
+    dfpatentgroup = dfcopy.groupby(['year',group]).sum()
+    dfpatentgroup=dfpatentgroup.unstack(level=1).fillna(0)
+    dfpatentgroup.sort_values(by=years,axis=1, ascending=False,inplace=True)
+    dfpatentgroup_copy=dfpatentgroup.transpose().copy()
+    dfpatentgroup_copy['total'] = dfpatentgroup_copy.apply(sum, axis=1)
+    dfpatentgroup_copy.reset_index(inplace=True)
+    dfpatentgroup_copy.drop('level_0', axis=1, inplace=True)
+    dfpatentgroup_copy.set_index(group, inplace=True)
+
+    if showtotal==False:
+        clean_df = dfpatentgroup_copy.sort_values(by='total', ascending=False).drop('total',axis=1)
+    else:
+        clean_df = dfpatentgroup_copy.sort_values(by='total', ascending=False)
+    return clean_df
+
+def read_df(PATH):
+    return pd.read_excel(PATH + '.xlsx')
+
+def plt_interactive_by_country(sector):
+    
+    feature = 'inventor_country'
+    years=list(range(2010,2017))
+
+    if sector == 'solar photo':
+        df=df_get_nb_by_group_and_clean(read_df('patent_solar_photo'), feature, years, False).head(10)
+    if sector == 'solar termal':
+        df=df_get_nb_by_group_and_clean(read_df('patent_solar_thermo'), feature, years, False).head(10)
+    if sector == 'wind':
+        df=df_get_nb_by_group_and_clean(read_df('patent_wind'), feature, years, False).head(10)
+    if sector == 'hydro':
+        df=df_get_nb_by_group_and_clean(read_df('patent_hydro'), feature,years, False).head(10)
+    if sector == 'tidal and wave':
+        df=df_get_nb_by_group_and_clean(read_df('patent_wave_tidal'), feature,years, False).head(10)
+    if sector == 'carbon capture':
+        df=df_get_nb_by_group_and_clean(read_df('patent_carbon_storage'), feature,years, False).head(10)
+    if sector == 'renewable':
+        df=df_get_nb_by_group_and_clean(read_df('patent_renewable'), feature, years, False).head(10)       
+    df.plot.barh(stacked=True, fontsize=20, figsize=(20,13), rot=0, grid=True)
+    plt.show()
+
+
+
+
+def plt_interactive_by_company(sector):
+    
+    feature = 'assignee_organization'
+    years=list(range(2010,2017))
+
+    if sector == 'solar photo':
+        df=df_get_nb_by_group_and_clean(read_df('patent_solar_photo'), feature, years, False).head(10)
+    if sector == 'solar termal':
+        df=df_get_nb_by_group_and_clean(read_df('patent_solar_thermo'),feature, years, False).head(10)
+    if sector == 'wind':
+        df=df_get_nb_by_group_and_clean(read_df('patent_wind'),feature, years, False).head(10)
+    if sector == 'hydro':
+        df=df_get_nb_by_group_and_clean(read_df('patent_hydro'), feature,years, False).head(10)
+    if sector == 'tidal and wave':
+        df=df_get_nb_by_group_and_clean(read_df('patent_wave_tidal'), feature,years, False).head(10)
+    if sector == 'carbon capture':
+        df=df_get_nb_by_group_and_clean(read_df('patent_carbon_storage'), feature,years, False).head(10)
+    if sector == 'renewable':
+        df=df_get_nb_by_group_and_clean(read_df('patent_renewable'), feature, years, False).head(10)       
+    df.plot.barh(stacked=True, fontsize=20, figsize=(20,13), rot=0, grid=True)
+    plt.show()
+
+
+def get_patents_keywords_energy(keywords,year, list_ipc):
+    ''' Returns all the patents containing the given keywords, for a given year'''
+
+    #Year query parameters
+    query_year = '{"_gte":{"patent_date":"'+year+'-01-01"}},{"_lt":{"patent_date":"'+str(int(year)+1)+'-01-01"}}'
+
+    nb_patents=0
+    dfPatents=pd.DataFrame()
+    #Send a query for every keyword
+    for (section, classification, group) in list_ipc:
+        query_group = '{"_eq":{"ipc_main_group":"'+group+'"}}'
+        query_section = '{"_eq":{"ipc_section":"'+section+'"}}'
+        query_classification = '{"_eq":{"ipc_class":"'+classification+'"}}'
+
+        query='q={"_and":['+query_year+','+query_section+','+query_classification+','+query_group+']}'
+        output='&f=["patent_title","inventor_country","assignee_organization","patent_number"]'
+        option='&o={"per_page":10000}'
+        #Exception handler in case no patent is found for a given keyword
+        try:
+            r = requests.get(BASE_URL()+query+output+option).json()
+            nb_patents+=pd.DataFrame(r).total_patent_count[0]
+            if nb_patents > 10000:
+                print('nb of patents too big (>10000)')
+            dfPatents=pd.concat([dfPatents,pd.DataFrame(r)],ignore_index=True)
+        except ValueError:
+            pass
+
+    #Clean the dataframe
+    dfPatents.reindex(list(range(len(dfPatents))))
+
+    columns = ["patent_title","patent_number","inventor_country","assignee_organization"]
+    dfPatents_cleaned=pd.DataFrame(columns=columns)
+
+    for col in columns:
+        if col == 'inventor_country':
+            dfPatents_cleaned[col]=list(map(lambda x: x['inventors'][0][col], dfPatents.patents))
+        elif col == 'assignee_organization':
+            dfPatents_cleaned[col]=list(map(lambda x: x['assignees'][0][col], dfPatents.patents))
+        else:
+            dfPatents_cleaned[col]=list(map(lambda x: x[col], dfPatents.patents))
+            
+    filter_list=[]
+    keyWordFound=False
+    for title in dfPatents_cleaned.patent_title:
+        for tuple_keyword in keywords:
+            for i in range(len(tuple_keyword)):
+                if tuple_keyword[i] not in title:
+                    keyWordFound=False
+                    break
+                if i == len(tuple_keyword)-1:
+                    keyWordFound=True
+            if keyWordFound==True:
+                break
+        filter_list+=[keyWordFound]
+
+    dfPatents_cleaned = dfPatents_cleaned.loc[filter_list]
+    return [dfPatents_cleaned, len(dfPatents_cleaned)]
+
+def get_nb_patent_years_keyword_energy(years,keywords,list_ipc):
+    list_patent_nb=[]
+    for i in years:
+        [dfPatent, nb_patent]=get_patents_keywords_energy(keywords,str(i),list_ipc)
+        list_patent_nb+=[nb_patent]
+    return list_patent_nb
